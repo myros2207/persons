269d360bb4063112b0084274c6db33f83fa68589
Myroslav Kurliak <92113420+myros2207@users.noreply.github.com>
2024-01-16 16:22:34 +0100

    1
    1

---
+++ ConsoleApp1/Osoba.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Osoba
+{
+    public string Imie { get; private set; }
+    public string Nazwisko { get; private set; }
+    public string Plec { get; private set; } //  pełne słowo
+    public DateTime DataUrodzenia { get; private set; }
+
+    public Osoba(string imie, string nazwisko, string plec, DateTime dataUrodzenia)
+    {
+        Imie = imie;
+        Nazwisko = nazwisko;
+        Plec = plec.ToLower() == "mężczyzna" ? "Mężczyzna" : "Kobieta";
+        DataUrodzenia = dataUrodzenia;
+    }
+
+
+    public void WyswietlInformacje()
+    {
+        Console.WriteLine($"Imię: {Imie}, Nazwisko: {Nazwisko}, Płeć: {Plec}, Data Urodzenia: {DataUrodzenia.ToShortDateString()}");
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Osoba osoba = new Osoba("Anna", "Nowak", "kobieta", new DateTime(1985, 7, 20));
+        osoba.WyswietlInformacje();
+
+        // Dodanie kolejnej osoby
+        Osoba innaOsoba = new Osoba("Paweł", "Kowalski", "mężczyzna", new DateTime(1992, 11, 15));
+        innaOsoba.WyswietlInformacje();
+    }
+}
+
